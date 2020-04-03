3eab3daae2885e696d7d5f75d36e5445133a4e54
Gui114ume <guillaume.lescroart@ens.uvsq.fr>
2020-04-03 21:03:19 +0200

    Prototype fonctions pour sha256

---
+++ Makefile
@@ -0,0 +1,18 @@
+CC=gcc
+CFLAGS=-g -Wall -O0
+LDFLAGS=-lm -L./ -lsha2
+EXEC=
+
+all: libsha2.so mysha2
+
+libsha2.so: libsha2.c
+	$(CC) -shared -fPIC $^ -o $@ $(CFLAGS)
+
+mysha2.o: main.c
+	$(CC) -c $^ -o $@ $(CFLAGS)
+
+mysha2 : mysha2.o libsha2.so
+	$(CC) $^ -o $@ $(LDFLAGS)
+
+clean:
+	 rm -rf *.o
---
+++ library.h
@@ -0,0 +1,86 @@
+#ifndef UNTITLED2_LIBRARY_H
+#define UNTITLED2_LIBRARY_H
+
+/*
+ * Pense bête !! ( x % 2^n )  ==  x & ( 2^n - 1 )
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+
+typedef unsigned long long u_int64;
+typedef unsigned int       u_int32;
+typedef unsigned char      BYTE;
+typedef unsigned int WORD_t;
+
+typedef struct BLOCK
+{
+    WORD_t word[16];
+} BLOCK_t;
+
+static const WORD_t H1_0 = 0x6a09e667;
+static const WORD_t H2_0 = 0xbb67ae85;
+static const WORD_t H3_0 = 0x3c6ef372;
+static const WORD_t H4_0 = 0xa54ff53a;
+static const WORD_t H5_0 = 0x510e527f;
+static const WORD_t H6_0 = 0x9b05688c;
+static const WORD_t H7_0 = 0x1f83d9ab;
+static const WORD_t H8_0 = 0x5be0cd19;
+
+u_int32 MsgPadding(FILE* fptr,
+                   u_int64 size);
+
+u_int64 GetSize(char* filename);
+
+//void Divide_M_InWord(BYTE* buffer,
+//                     WORD_t* W);
+
+// alloue W, puis calcule et rempli ce tableau W
+void PreComputeW(BYTE* buffer,
+                 WORD_t* W);
+
+void BinToHexString(WORD_t* res_word,
+                    char* hash);
+
+void InitRegisters(WORD_t* registers, // un tableau de 8 WORD_t ( taille de H, intermédiate hash value)
+                   WORD_t* H);
+
+void SHA256_CompressionFunction(WORD_t* registers,
+                                BYTE* buffer);
+
+void ComputeIntermediateHash(WORD_t* H,
+                             WORD_t* registers);
+
+void InitK(WORD_t* K);
+
+WORD_t Sn(WORD_t x,
+          u_int32 n);
+
+WORD_t Rn(WORD_t x,
+          u_int32 n);
+
+WORD_t Ch(WORD_t x,
+          WORD_t y,
+          WORD_t z);
+
+WORD_t Maj(WORD_t x,
+           WORD_t y,
+           WORD_t z);
+
+WORD_t E0(WORD_t x);
+
+WORD_t E1(WORD_t x);
+
+WORD_t sig0(WORD_t x);
+
+WORD_t sig1(WORD_t x);
+
+void RemoveAddedBytes(FILE* fptr,
+                      u_int64 size);
+
+
+#endif //UNTITLED2_LIBRARY_H
